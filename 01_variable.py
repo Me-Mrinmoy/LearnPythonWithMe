b65ab5e9f8d856a466ed142872f271a10b75ad40
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-01-10 18:00:31 +0530

    Add files via upload

---
+++ 01_variable.py
@@ -0,0 +1,17 @@
+a = "mrinmoy"
+b= 345
+c = 45.32
+d = True
+# d = None
+
+# printing the variables
+print(a)
+print(b)
+print(c)
+
+# printing the type of varibles
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
