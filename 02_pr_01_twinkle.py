c1eca6971d65f18355b5a179f808127b3d66e1b3
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-01-21 19:57:09 +0530

    Add files via upload

---
+++ 02_pr_01_twinkle.py
@@ -0,0 +1,18 @@
+# Write a program to print twinkle twinkle little star poem in python
+
+#This is  wrong--
+'''print("Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.")
+'''
+
+'''This problem is a solution of code with harry practice set1
+'''
+
+
+#This is right---
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky''')
