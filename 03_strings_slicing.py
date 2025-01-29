d3b7b85ba43ea322b67062db5c15ca673500e094
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-01-29 13:10:45 +0530

    Add files via upload

---
+++ 03_strings_slicing.py
@@ -0,0 +1,19 @@
+greeting = "Good Morning,"
+# name = "Mrinmoy"
+# print(type(name))
+# Concatenating two strings
+# print(greeting+name)
+# c = greeting + name
+# print(c)
+name = "MrinmoyIsGood"
+print(name[4])
+# name[3] = "d"  -->does not work
+
+# print(name[1:4])  # That's slicing
+# print(name[:4] )   # is same as name[0:4]
+# print(name[1:])     # is same as name[1:5]
+# c = name[-4:-1]      # is same as name[1:4]
+# print(c)
+
+#d = name[0::2]      skip two alphabat 
+# print(d)
