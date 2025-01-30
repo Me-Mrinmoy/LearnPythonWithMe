698831db017c3c4da513878d2907eefe1e629c0b
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-01-30 19:03:10 +0530

    Add files via upload

---
+++ 03_typecasting.py
@@ -0,0 +1,13 @@
+# type casting convert one data type to another data type
+
+# this is right--
+a ="3534"
+a = int(a)
+# print(type(a))
+print(a+5)
+
+# this is wrong--
+a = "35fgrfg34"
+a  = int(a)
+# print(type(a))
+print(a+5)
