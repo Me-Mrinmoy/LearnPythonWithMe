2539bd96f0656aa4b140119e358fd8f19b90f587
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-01-31 12:00:51 +0530

    Add files via upload

---
+++ List/04_list.py
@@ -0,0 +1,21 @@
+# Create a list using[]
+a = [1,2,3,4,56,6]
+
+# print the list using print() funcion
+print(a)
+
+# Access using index using a[0], a[1], a[2]
+print(a[2])
+
+# Change the value of list using
+a[0] = 98
+print(a)
+
+# We can  create a list with items of different tyoes
+c = [45,"Mrinmoy",False,6.9]
+print(c)
+
+# List slicing
+friends = ["Mrinmoy","Nayan", "Apurba", "Nilanjan",45]
+print(friends[0:4])
+print(friends[-4:])
