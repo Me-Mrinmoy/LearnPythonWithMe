4d401fc129b89208c1680fecf7a178755ef333f1
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-06 10:55:05 +0530

    Add files via upload

---
+++ Chapter - 2 Practice Set/practiceSet2_04comparison.py
@@ -0,0 +1,6 @@
+'''Use comparison operators to find out wheather a given variable a is greater than b or not (Take a =34, b = 80)'''
+
+a = 34
+b = 80
+# print(a>b)  # its false
+print(b>a)  # its true
