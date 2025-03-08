21c900425916fb29cef67052d044b6e9ad43c7a3
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-08 08:44:33 +0530

    Add files via upload

---
+++ Chapter - 2 Practice Set/practiceSet2_06_calculatespuareofnum.py
@@ -0,0 +1,11 @@
+# Write a python programe to calculate square of a number entered by the user---
+
+
+# Get the number from the user
+a = int(input("Enter a number: "))
+
+# Calculate the square
+square = a * a
+
+# Print the result
+print("The square of a number is:",square)
