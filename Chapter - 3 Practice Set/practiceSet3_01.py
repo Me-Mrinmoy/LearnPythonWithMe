8dde41dcd4ad9589b4c8c4a6c3ec167b5af53d3a
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-09 08:33:50 +0530

    Add files via upload

---
+++ Chapter - 3 Practice Set/practiceSet3_01.py
@@ -0,0 +1,6 @@
+'''Q1.Write a python program to display a user
+entered name followed by Good Afernoon using input() function'''
+
+
+name = input("Enter your name\n")
+print("Good afternoon,"+name)
