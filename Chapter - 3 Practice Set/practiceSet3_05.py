6b20138c2bc35433c279b8381da0698acf69bdd3
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-10 08:49:49 +0530

    Add files via upload

---
+++ Chapter - 3 Practice Set/practiceSet3_05.py
@@ -0,0 +1,10 @@
+'''Q5.Write a program to format the following letter 
+using escape sequence charracter
+letter = "Dear Mrinmoy, this python course is nice. Thanks!'''
+
+
+letter = "Dear Mrinmoy this python course is nice! Thanks!"
+print(letter)
+
+formatted_letter = "Dear Mrinmoy,\n\t This python course is nice!\nThanks"
+print(formatted_letter)
