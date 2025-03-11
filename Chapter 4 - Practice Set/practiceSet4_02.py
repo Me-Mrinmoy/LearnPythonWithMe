4bae2e31ae76cc2c71a90845f32d874e2cca9ece
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-11 08:57:08 +0530

    Add files via upload

---
+++ Chapter 4 - Practice Set/practiceSet4_02.py
@@ -0,0 +1,13 @@
+'''Q2.Write a program to accept marks of 6
+students and display them in a sorted manner'''
+
+m1 = input("Enter marks for Student Number 1:")
+m2 = input("Enter marks for Student Number 2:")
+m3 = input("Enter marks for Student Number 3:")
+m4 = input("Enter marks for Students Number 4:")
+m5 = input("Enter marks for Student Number 5:")
+m6 = input("Enter marks for Student Number 6:")
+
+myList = [m1,m2,m3,m4,m5,m6]
+
+print(myList)
