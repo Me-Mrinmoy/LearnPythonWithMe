3797c3e9dfb1f74116f8e0a3191aa8b37acf1a86
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-18 07:55:59 +0530

    Add files via upload

---
+++ Chapter 5 - Practice Set/practiceSet5_03.py
@@ -0,0 +1,5 @@
+'''Q3.Can we have a set with 18(int) and "18"(str) as a values in it '''
+
+s = {18,"18",18.1}
+print(s)
+# Set will print the three values there 18 is integar and "18"is string and 18.1 is floating number
