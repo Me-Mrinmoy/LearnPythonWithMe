bd6a2d247edb6c4c44d4a820d8b4bd30943e3925
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-21 08:57:46 +0530

    Add files via upload

---
+++ Chapter 5 - Practice Set/practiceSet5_08.py
@@ -0,0 +1,17 @@
+'''Q8.If language of two friends are same, what will
+happen to the program in problem 6.'''
+
+
+favlang = {}
+
+a = input("Enter your favourite language Nayan\n")
+b = input("Enter your favourite language Apurba\n")
+c = input("Enter your favourite language Bisha\n")
+d = input("Enter your favourite language Esha\n")
+
+favlang['Nayan'] = a
+favlang['Apurba'] = b
+favlang['Bisha'] = c
+favlang['Esha'] = d
+
+print(favlang)
