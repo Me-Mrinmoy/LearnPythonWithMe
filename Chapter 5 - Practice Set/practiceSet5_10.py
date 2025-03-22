70341c5c436d4d813736680e76b934dc302418f4
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-22 09:00:55 +0530

    Add files via upload

---
+++ Chapter 5 - Practice Set/practiceSet5_10.py
@@ -0,0 +1,18 @@
+'''Write a programe to creare an empty dictionary.In the dictionary 
+store it the hindi words with values in their english translation.'''
+
+dict = {
+
+}
+
+a = input ("Enter the hindi word of fan: ")
+b = input ("Enter the hindi word of box: ")
+c = input ("Enter the hindi word of item: ")
+d = input ("Enter the hindi word of book: ")
+
+dict['fan'] = a
+dict['box'] = b
+dict['item'] = c
+dict['book'] = d
+
+print (dict)
