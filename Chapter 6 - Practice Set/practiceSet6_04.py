59ffc70809db978177bc1e6368c7e192bbf10ce2
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-24 07:27:01 +0530

    Add files via upload

---
+++ Chapter 6 - Practice Set/practiceSet6_04.py
@@ -0,0 +1,10 @@
+'''Q4. Write a program to find wheather a given username 
+contains less than 10 charracter or not'''
+
+
+name = input("Enter your name")
+# print(len(name))
+if(len(name)<10):
+    print("Username is not  10 charracter")
+else:
+    print("Username is 10 charracter")    
