4cda640aeed4e6e923e69c42209977d2e8572080
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-24 07:27:33 +0530

    Add files via upload

---
+++ Chapter 6 - Practice Set/practiceSet6_05.py
@@ -0,0 +1,11 @@
+'''Q5. Write a program which finds out wheather a given 
+name is present in a list or not. '''
+
+
+names = ["mrinmoy","apurba","nayan","biswajit","aranya","jayanta"]
+name = input("Enter the name to check\n")
+
+if name in names:
+      print("Your name is present in the lst")
+else:
+      print("Your name is not present in the list")      
