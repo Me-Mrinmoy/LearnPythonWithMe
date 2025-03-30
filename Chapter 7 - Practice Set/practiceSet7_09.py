0f1b7450ba202952107a7d9caaffdedfaba14957
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-03-30 06:54:07 +0530

    Add files via upload

---
+++ Chapter 7 - Practice Set/practiceSet7_09.py
@@ -0,0 +1,15 @@
+'''Q9. Write a program to print
+the following star pattern (for n = 3)
+* * *
+*   *
+* * *
+'''
+
+n = 3  # Number of rows is fixed to 3
+
+# Predefine the rows
+rows = ["* * *", "*   *", "* * *"]
+
+# Use a for loop to print each row
+for row in rows:
+    print(row)
