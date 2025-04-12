47b66df7ac8559e39e629a65d3082289555695cd
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-04-12 09:30:00 +0530

    Add files via upload

---
+++ Conditional Statement/06_conditional.py
@@ -0,0 +1,25 @@
+#1.if_elif_else leader in python--
+'''a = 45
+if(a>3):
+
+    print("The value a is greater than 3")
+elif(a>7):
+    print("The value of a is grater than 7")
+else:
+    print("The value is not greater than 3 or 7")
+'''
+      
+#2. Multiple if statements in pyhton--
+'''a = 8
+if(a>3):
+    print("The value of  a is greater than 3")
+if(a>7):
+    print("The value of a is greater than 7")
+if(a>13):
+    print("The value of a is greater than 13")
+if(a>17):
+    print("The value of a is greater than 17")
+else:
+    print("The value is not greater than 3 or 7")        
+'''
+
