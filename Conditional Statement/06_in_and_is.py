51752dc193bb93c2c58549ba4b89625c7bd0d1a2
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-04-13 07:37:16 +0530

    Add files via upload

---
+++ Conditional Statement/06_in_and_is.py
@@ -0,0 +1,17 @@
+# Using is in python---
+'''a  = None
+if(a is None):
+    print("Yes")
+else:
+    print("no")'''    
+
+
+# Using in in python---
+a = [45,56,7]
+print(45 in a)  # its returns true because 45 present in a
+
+print(454 in a) # its returns false because 454 is not present in a
+
+
+   
+   
