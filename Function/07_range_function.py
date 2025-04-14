c9e9e7fc1a51c20a682706e928e9780e7b31aec6
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-04-15 01:03:57 +0530

    Add files via upload

---
+++ Function/07_range_function.py
@@ -0,0 +1,13 @@
+# Here my code prints 1 to 8 numbers--
+for i in range(1,8):
+    print(i)
+
+# Here my code prints 2 to 8 numbers--
+for i in range(2,8):
+    print(i)
+
+ # Here my codes prints 1,3,5,7 numbers the difference between 2 from 1 to 8 numbers--  
+for i in range(1,8,2):
+    print(i)
+
+   #that's how the range function works in python
