2946e08bbce1d5c6c1742c3bb3ebc69d9fcc4962
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-04-14 07:51:52 +0530

    Add files via upload

---
+++ Dictionary/05_dictionary_syntex.py
@@ -0,0 +1,11 @@
+myDict ={
+"Fast": "In a quick maanner",
+"Mrinmoy":"A coder",
+"Marks": [1,2,5],
+"anotherdict":{'mrinmoy': 'player'}
+}
+
+print(myDict['Fast'])
+print(myDict['Mrinmoy'])
+print(myDict['Marks'])
+print(myDict['anotherdict']['mrinmoy'])
